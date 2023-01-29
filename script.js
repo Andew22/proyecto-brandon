0f1e5498a2db1b3f3b59d8f718a4a878612da6fd
Andrew <84572601+Andew22@users.noreply.github.com>
2023-01-28 20:31:06 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,87 +1,76 @@
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
+let contenedor = document.querySelector(".contenedor_tabla");
+let max_element = 0;
+let array_final = [];
+let inputBuscar = document.querySelector(".input_buscar");
+let array_principal =  [];
+let arrays = [1,2,3,4,5,6,7,8,9]
 
-readTextFile("myfile.json", function(text){
-    var data = JSON.parse(text);
-
-    var new_datos = data.split(",");
-
-    var contador = 1;
-
-    var base = document.querySelector(".contenedor_tabla");
-
-
-    var contenidO = `     
-    `;
-
-    while(contador!=200){
-
-        var elemento = new_datos[contador];
-        var elementos = elemento.split(";");
-
-        var elemento1 = elementos[0].replace("[","");
-        var elemento2 = elemento1.replace(/["']/g,'');
-        var elemento3 = elemento2.replace("]","");
-
-        var elemento4 = elementos[1].replace("[","");
-        var elemento5 = elemento4.replace(/["']/g,'');
-        var elemento6 = elemento5.replace("]","");
-
-
-        var elemento7 = elementos[2].replace("[","");
-        var elemento8 = elemento7.replace(/["']/g,'');
-        var elemento9 = elemento8.replace("]","");
-
-
-
-        var parte1 = document.createElement("tr");
-        var parte2 = document.createElement("th");
-        var parte3 = document.createElement("td");
-        parte3.innerText = elemento3;
-        var parte4 = document.createElement("td");
-        parte4.innerText = elemento6;
-        var parte5 = document.createElement("td");
-        parte5.innerText = elemento9;
-
-
-        parte1.appendChild(parte3);
-        parte1.appendChild(parte4);
-        parte1.appendChild(parte5);
-        
-
-
-        base.appendChild(parte1);
-
-
-        contador++;
-    }
-
-
- 
-  
-     
-     
-        
-   
-
-
-    
-    
-
-    
- 
-
+fetch('myfile.json')
+    .then(response => response.json())
+    .then((data) => {
+     array_principal = data.split(",");
 
 });
+
+
+function buscarRegistros(){
+    eliminarElementos();
+    let texto = inputBuscar.value;
+    const result = array_principal.filter(element => element.split(";")[0].includes(texto,0));
+    pintarElementos(result);
+}
+
+
+function limpiarDatos(elemento){
+    if(elemento!=undefined){
+        let paso1 = elemento.replace("[","");
+        let paso2 = paso1.replace("]","");
+        let paso3 = paso2.replace(/[ '"]+/g,"");
+        return paso3;
+    }
+}
+
+
+function pintarElementos(result){
+    let maxElement0s = 10;
+    let elementos = 0;
+    for (var i = 0; i < 10; i++) {
+        let elementos = result[i].split(";");
+        contenedor.appendChild(crearElemento(limpiarDatos(elementos[0]),limpiarDatos(elementos[1]),limpiarDatos(elementos[2])));
+
+
+    }
+
+}
+
+
+
+function crearElemento(a1,a2,a3){
+
+    let elemento1 = document.createElement("tr");
+    elemento1.classList.add("elemento");
+    let elemento2 = document.createElement("th");
+    elemento2.innerHTML = a1;
+    let elemento3 = document.createElement("td");
+    elemento3.innerHTML = a2;
+    let elemento4 = document.createElement("td");
+    elemento4.innerHTML = a3;
+
+    elemento1.appendChild(elemento2);
+    elemento1.appendChild(elemento3);
+    elemento1.appendChild(elemento4);
+
+
+    return elemento1;
+
+}
+
+function eliminarElementos(){
+    let elementos = document.querySelectorAll(".elemento");
+    elementos.forEach(element => {
+        element.remove();
+    });
+}
 
 
 
